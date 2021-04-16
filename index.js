6a4b4e1ea5e2537b826d65d9359ffb94a088dfe3
Guiwoo Park <guiwoopark@Guiwooui-MacBookAir.local>
2021-04-16 17:22:46 +0900

    type roll

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const meow = "Meow coin", age = 29, gender = "male";
+const sayHi = (meow, age, gender) => {
+    console.log(`Hello ${meow}, you are ${age} old, you are a ${gender}`);
+};
+sayHi(meow, age, gender);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,11 @@
+const meow = "Meow coin",
+    age = 29,
+    gender = "male";
+
+const sayHi = (meow,age,gender)=>{
+    console.log(`Hello ${meow}, you are ${age} years old, your gender is ${gender}`)
+}
+
+sayHi(meow,age,gender);
+
+export {};
