08dbc836dc815c5117f1165ab6b9b79c59ed312a
Guiwoo Park <guiwoopark@Guiwooui-MacBookAir.local>
2021-04-18 16:35:51 +0900

    contain_typescript

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const meow = "Meow coin", age = 29, gender = "male";
-const sayHi = (meow, age, gender) => {
-    console.log(`Hello ${meow}, you are ${age} old, you are a ${gender}`);
-};
-sayHi(meow, age, gender);
-//# sourceMappingURL=index.js.map
